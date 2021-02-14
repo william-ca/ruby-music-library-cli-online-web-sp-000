9b369c0a2ce13d9142f46c01567972e08f2e1209
Will Andre <wandre94@gmail.com>
2021-02-14 00:42:50 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -70,11 +70,24 @@ class MusicLibraryController
  end
 
   def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort{ |a, b| a.name <=> b.name}.each_with_index do |s, i|
+        puts "#{i+1}. #{s.artist.name} - #{s.name}"
+      end
+    end
   end
 
   def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+    if input > 0 && input <= Song.all.length
+      array = Song.all.sort{ |a, b| a.name <=> b.name }
+      song = array[input-1]
+      puts "Playing #{song.name} by #{song.artist.name}"
+    end
   end
-
-
 
 end
